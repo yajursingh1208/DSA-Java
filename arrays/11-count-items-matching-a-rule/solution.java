fad272353efdd468a9d4a78cb723a2b959435d9d
Yajur Singh <yajursingh1208@Yajurs-MacBook-Air.local>
2026-08-23 16:10:37 +0530

    Solve LeetCode 1773

---
+++ arrays/11-count-items-matching-a-rule/solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+    int index;
+    if(ruleKey.equals("type")){
+        index = 0;
+    }
+    else if(ruleKey.equals("color")){
+        index = 1;
+    }
+    else{index = 2;}    
+    
+    int count = 0;
+    for(int i =0; i<items.size(); i ++){
+        if(items.get(i).get(index).equals(ruleValue)){
+            count++;
+        }
+    }return count;}
+}
