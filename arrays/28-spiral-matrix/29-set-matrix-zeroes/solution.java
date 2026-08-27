354d08d42cff52183a5676ed0b564f8428a244c9
Yajur Singh <yajursingh1208@Yajurs-MacBook-Air.local>
2026-08-27 18:13:40 +0530

    Solve leetcode 73

--- arrays/28-spiral-matrix/29-set-matrix-zeroes/solution.java
+++ arrays/28-spiral-matrix/29-set-matrix-zeroes/solution.java
@@ -22,3 +22,26 @@ class Solution {
                 matrix[k][col] = 0;}
     }
 }}}
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+    boolean [] rows = new boolean [matrix.length];
+    boolean [] columns = new boolean [matrix[0].length];   
+    for(int i =0; i<matrix.length; i ++){
+        for(int j =0; j<matrix[0].length; j++){
+            if(matrix[i][j] == 0){
+                rows[i] = true;
+                columns[j] = true;
+            }
+        }
+    }
+    for(int i =0; i<matrix.length; i++){
+        for (int j = 0; j < matrix[0].length; j++) {
+                if(rows[i] || columns[j]){
+                    matrix[i][j] = 0;
+                }
+        
+    }
+    }
+}}
