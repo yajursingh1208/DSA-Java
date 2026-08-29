644ed82b5d7a0d7237a0c90e04cb0c6e8bec3a98
Yajur Singh <yajur.singh.ug25@nsut.ac.in>
2026-08-29 12:48:19 +0530

    Solve leetcode 283

---
+++ arrays/33-move-zeroes/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+    int left = 0;
+    int right = 0;
+    for(right = 0; right< nums.length; right++){
+        if(nums[right]>0){
+            int temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+            left ++;
+        }
+    }  
+    }
+}
