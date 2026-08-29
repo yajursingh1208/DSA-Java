761203579ec2ac52185764b08c01cc1280b074da
Yajur Singh <yajur.singh.ug25@nsut.ac.in>
2026-08-29 14:55:49 +0530

    Solve leetcode 189

--- arrays/35-rotate-array/solution.java
+++ arrays/35-rotate-array/solution.java
@@ -16,3 +16,20 @@ class Solution {
         }
     }
 }
+
+# APPROACH-2
+
+class Solution {
+    public void rotate(int[] nums, int k) {
+    int n = nums.length;
+    k = k%n;  
+    int[] ans = new int [n];
+    for(int i =0; i<n; i++){
+        int newindex = (i+k)%n;
+        ans[newindex] = nums[i];
+    }
+    for(int i =0; i<n; i++){
+        nums[i] = ans[i];
+    }
+    }
+    }
