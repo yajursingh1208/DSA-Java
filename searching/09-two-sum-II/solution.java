069c55f421b1167f936f93c1ecc306217ed4f4e5
Yajur Singh <yajur.singh.ug25@nsut.ac.in>
2026-08-30 20:57:26 +0530

    Solve leetcode 167

--- searching/09-two-sum-II/solution.java
+++ searching/09-two-sum-II/solution.java
@@ -13,3 +13,25 @@ class Solution {
     return ans;  
     }
 }
+
+// approach 2: 2 pointer method
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+    int left = 0;
+    int right = numbers.length-1;
+    int ans [] = new int [2];
+    while(left<right){
+        if(numbers[left]+numbers[right]==target){
+            ans[0] = left + 1;
+            ans[1] = right + 1;
+            return ans;
+        }
+        else if(numbers[left]+numbers[right]<target){
+            left++;
+        }
+        else{
+            right--;
+        }
+    } return ans; 
+    }
+}
