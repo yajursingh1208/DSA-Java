38bf0a8df76702e60e80fe22780963c940f85f13
Yajur Singh <yajur.singh.ug25@nsut.ac.in>
2026-08-30 10:49:48 +0530

    Solve leetcode 268

--- arrays/39-missing-number/solution.java
+++ arrays/39-missing-number/solution.java
@@ -10,3 +10,14 @@ class Solution {
     return missing;    
     }
 }
+
+//approach 2
+class Solution {
+    public int missingNumber(int[] nums) {
+    int xor = nums.length;
+    for(int i =0; i<nums.length; i++){
+        xor = xor^i^nums[i];
+    }   
+    return xor;
+    }
+}
