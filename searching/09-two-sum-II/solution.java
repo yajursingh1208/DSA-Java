c02ff19527dac7a5c5b37bb71adf5ec362bb7d55
Yajur Singh <yajur.singh.ug25@nsut.ac.in>
2026-08-30 20:41:57 +0530

    Solve leetcode 167

---
+++ searching/09-two-sum-II/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+    int ans [] = new int[2];
+    for(int i = 0; i<numbers.length; i++){
+        for(int j = i+1; j<numbers.length; j++){
+            if(numbers[j] == target - numbers[i]){
+                ans[0] = i+1;
+                ans[1] = j+1;
+                return ans;
+            }   
+        }
+    }   
+    return ans;  
+    }
+}
