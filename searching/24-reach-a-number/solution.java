4d93be1f411f0f1531df1eed02c96cdaa8070d62
Yajur Singh <yajur.singh.ug25@nsut.ac.in>
2026-09-04 15:07:05 +0530

    Solve leetcode 754

---
+++ searching/24-reach-a-number/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int reachNumber(int target) {
+    target = Math.abs(target);
+    int sum = 0;
+    int steps = 0;
+    while(target > sum){
+        steps ++;
+        sum = sum + steps;
+    }   
+    while((sum-target) % 2 != 0){
+        steps ++;
+        sum = sum + steps;
+    }
+    return steps;     
+    }
+}
