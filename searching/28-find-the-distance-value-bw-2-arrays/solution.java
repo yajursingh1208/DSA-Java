9a7e93aa9b4d9b7e269b7d620d1fea1bdd6a13b6
Yajur Singh <yajur.singh.ug25@nsut.ac.in>
2026-09-05 15:51:47 +0530

    Solve leetcode 1385

--- searching/28-find-the-distance-value-bw-2-arrays/solution.java
+++ searching/28-find-the-distance-value-bw-2-arrays/solution.java
@@ -16,3 +16,30 @@ class Solution {
     return count;
     }
 }
+
+//approach 2
+class Solution {
+    public int findTheDistanceValue(int[] arr1, int[] arr2, int d) {
+    Arrays.sort(arr2);
+    int count = 0;
+    for(int i = 0; i< arr1.length; i++){
+        int low = arr1[i] - d;
+        int high = arr1[i] + d;
+        int start = 0;
+        int end = arr2.length - 1;
+        while(start <= end){
+            int mid = start + (end - start)/2;
+            if(arr2[mid] < low){
+                start = mid + 1;
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+        if(start == arr2.length || arr2[start] > high){
+            count++;
+        }
+    } 
+    return count; 
+    }
+}
